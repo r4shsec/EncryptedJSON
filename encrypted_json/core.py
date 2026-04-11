217522768ad47bd08c8a7054a3c571bf08d2d194
Rash <r4shsec@protonmail.com>
2026-04-11 13:04:50 +0800

    Made improvements

--- encrypted_json/core.py
+++ encrypted_json/core.py
@@ -11,17 +11,29 @@ def encrypt(file, key):
     """
     Load the unencrypted JSON file and encrypt the data.
     """
-    with open(file, 'r') as f:
-        data = json.load(f)
-    ready_data = json.dumps(data).encode()
-    f_enc = Fernet(make_key(key))
-    return f_enc.encrypt(ready_data)
+    try:
+        with open(file, 'r') as f:
+            data = json.load(f)
+    except Exception as e:
+        raise Exception(f"Error loading JSON file: {e}")
+    try:
+        ready_data = json.dumps(data).encode()
+        f_enc = Fernet(make_key(key))
+        return f_enc.encrypt(ready_data)
+    except Exception as e:
+        raise Exception(f"Error encrypting data: {e}")
 
 def decrypt(file, key):
     """
     Load the encrypted EJSON file and decrypt the data.
     """
-    with open(file, 'r') as f:
-        data = f.read().encode()
-    f_enc = Fernet(make_key(key))
-    return json.loads(f_enc.decrypt(data).decode())
+    try:
+        with open(file, 'r') as f:
+            data = f.read().encode()
+    except Exception as e:
+        raise Exception(f"Error loading encrypted file: {e}")
+    try:
+        f_enc = Fernet(make_key(key))
+        return json.loads(f_enc.decrypt(data).decode())
+    except Exception as e:
+        raise Exception(f"Error decrypting data: {e}")
